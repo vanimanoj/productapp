55ba6875b1694195d13a6be0878c4e2f4510e174
vaniammu@123 <vanimanoj1997@gmail.com>
2022-05-18 00:11:15 +0530

    search

--- src/App.js
+++ src/App.js
@@ -2,24 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import Productentery from './component/Productentery';
+import Productsearch from './component/Productsearch';
+import Viewproduct from './component/Viewproduct';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+    <BrowserRouter>
+    <Routes>
+<Route path="/" exact element={<Productentery/>}/>
+<Route path="/search" exact element={<Productsearch/>}/>
+<Route path="/view" exact element={<Viewproduct/>}/>
+
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
---
+++ src/component/Productsearch.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Productsearch = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div class="container">
+    <div class="row">
+        <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div class="row g-3">
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" class="form-label">Product Name</label>
+                    <input type="text" class="form-control"/>
+                </div>
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button class="btn btn-success">SEARCH</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
+  )
+}
+
+export default Productsearch
